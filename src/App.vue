7bf86d4f44b69e2d949615209c28b5307331fc4e
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-08-18 16:16:24 -0300

    Feat: creating the input card

--- src/App.vue
+++ src/App.vue
@@ -20,23 +20,13 @@ export default {
 html,
 body,
 #app {
-  widows: 100vw;
   height: 100vh;
+  background: linear-gradient(116.82deg, rgba(247, 183, 24, 0.7) 0%, rgba(245, 197, 78, 0.6) 100%);
 }
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
-  background: linear-gradient(116.82deg, rgba(247, 183, 24, 0.7) 0%, rgba(245, 197, 78, 0.6) 100%);
-}
-.disable-select {
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-button {
-  cursor: pointer;
 }
 </style>
--- src/components/micro/Inputs/Inputs.vue
+++ src/components/micro/Inputs/Inputs.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-    <input type="range" min="10" max="180">
-    <input type="range" min="1.00" max="2.50" step="any">
+    <div id="inputs">
+        <div class="formatInput" v-if="type === 'peso'">
+            <div class="formatCard">
+                <p>Peso</p>
+                <p class="formatValue">65</p>
+            </div>
+            <input type="range" min="10" max="180">
+        </div>
+        <div class="formatInput" v-else-if="type === 'altura'">
+            <div class="formatCard">
+                <p>Altura</p>
+                <p class="formatValue">1.64</p>
+            </div>
+            <input type="range" min="1.00" max="2.50" step="any">
+        </div>
     </div>
 </template>
 
@@ -9,7 +21,14 @@
 
 
 export default {
-
+    // eslint-disable-next-line
+    name: "Inputs",
+    props:{
+        type:{
+            type: String,
+            default: "peso"
+        }
+    }
 }
 </script>
 
